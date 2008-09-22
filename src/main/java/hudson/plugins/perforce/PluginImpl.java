0229ecc6158be81faadfe25277fb8b0d0d2a146b
thepner <thepner@dev.java.net>
2008-09-22 22:22:29 +0000

    cleanup unused imports

--- src/main/java/hudson/plugins/perforce/PluginImpl.java
+++ src/main/java/hudson/plugins/perforce/PluginImpl.java
@@ -1,10 +1,8 @@
 package hudson.plugins.perforce;
 
 import hudson.Plugin;
-import hudson.scm.SCM;
 import hudson.scm.SCMS;
 import hudson.scm.RepositoryBrowsers;
-import hudson.tasks.BuildStep;
 import hudson.tasks.MailAddressResolver;
 
 import hudson.plugins.perforce.browsers.*;
