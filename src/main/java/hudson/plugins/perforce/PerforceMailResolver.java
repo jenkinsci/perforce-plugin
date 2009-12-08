55b7e7cc27079ccbeca0577af9ea9f932ecd4971
rpetti <rpetti@dev.java.net>
2009-12-08 22:04:21 +0000

    [FIXED HUDSON-4933] fixed perforce email resolution. It should now work normally again.

--- src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
+++ src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
@@ -21,29 +21,23 @@ public class PerforceMailResolver extends MailAddressResolver {
     @SuppressWarnings("unchecked")
 	public String findMailAddressFor(User u) {
     	
-    	/*
-		The previous implementation only generated NPEs for me, and this one just hangs.  So I'm
-		just going to leave it as a no-op on the theory that it's no worse than I found it.
-		<http://www.nabble.com/PerforceMailResolver-is-broken--td25934348.html>
-		*/
-//        for (AbstractProject p : u.getProjects()) {
-//
-//            if (p.getScm() instanceof PerforceSCM) {
-//                PerforceSCM pscm = (PerforceSCM) p.getScm();
-//                TaskListener listener = new StreamTaskListener(System.out);
-//                try {
-//                    // I'm not sure if this is the standard way to create an ad-hoc Launcher, I just
-//                	// copied it from HudsonP4Executor.exec
-//                	Launcher launcher = Hudson.getInstance().createLauncher(listener);
-//                    com.tek42.perforce.model.User pu = pscm.getDepot(launcher, p.getWorkspace()).getUsers().getUser(u.getId());
-//                    if (pu.getEmail() != null && !pu.getEmail().equals(""))
-//                        return pu.getEmail();
-//
-//                } catch (Exception e) {
-//                    listener.getLogger().println("Could not get email address from Perforce: "+e.getMessage());
-//                }
-//            }
-//        }
+        for (AbstractProject p : Hudson.getInstance().getProjects()) {
+            if (p.getScm() instanceof PerforceSCM) {
+                PerforceSCM pscm = (PerforceSCM) p.getScm();
+                TaskListener listener = new StreamTaskListener(System.out);
+                try {
+                    // I'm not sure if this is the standard way to create an ad-hoc Launcher, I just
+                	// copied it from HudsonP4Executor.exec
+                	Launcher launcher = Hudson.getInstance().createLauncher(listener);
+                    com.tek42.perforce.model.User pu = pscm.getDepot(launcher, p.getWorkspace()).getUsers().getUser(u.getId());
+                    if (pu.getEmail() != null && !pu.getEmail().equals(""))
+                        return pu.getEmail();
+
+                } catch (Exception e) {
+                    listener.getLogger().println("Could not get email address from Perforce: "+e.getMessage());
+                }
+            }
+        }
         return null;
     }
 
