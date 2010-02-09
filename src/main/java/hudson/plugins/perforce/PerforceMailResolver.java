d141b90f10cd41e4ae478ce91636b4cc9c689a14
rpetti <rpetti@dev.java.net>
2010-02-09 21:42:21 +0000

    removing deprecated call to AbstractProject.getWorkspace in perforce mail resolver.

--- src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
+++ src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
@@ -1,13 +1,16 @@
 package hudson.plugins.perforce;
 
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Hudson;
 import hudson.model.TaskListener;
 import hudson.model.User;
 import hudson.tasks.MailAddressResolver;
 import hudson.util.StreamTaskListener;
+import java.io.File;
 
 /**
  * Implementation of {@link MailAddressResolver} for looking up the email address of a user in the Perforce repository.
@@ -27,9 +30,10 @@ public class PerforceMailResolver extends MailAddressResolver {
                 TaskListener listener = new StreamTaskListener(System.out);
                 try {
                     // I'm not sure if this is the standard way to create an ad-hoc Launcher, I just
-                	// copied it from HudsonP4Executor.exec
-                	Launcher launcher = Hudson.getInstance().createLauncher(listener);
-                    com.tek42.perforce.model.User pu = pscm.getDepot(launcher, p.getWorkspace()).getUsers().getUser(u.getId());
+                    // copied it from HudsonP4Executor.exec
+                    FilePath workspace = p.getLastBuiltOn().createPath(p.getLastBuiltOn().getRootPath().getRemote());
+                    Launcher launcher = Hudson.getInstance().createLauncher(listener);
+                    com.tek42.perforce.model.User pu = pscm.getDepot(launcher, workspace).getUsers().getUser(u.getId());
                     if (pu.getEmail() != null && !pu.getEmail().equals(""))
                         return pu.getEmail();
 
