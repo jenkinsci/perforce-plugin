7e96ec96908f8b300797837da86ad041a836884a
rpetti <rpetti@dev.java.net>
2010-03-26 15:05:41 +0000

    [FIXED HUDSON-6079] added error handling for perforcemailresolver when the last node the job was run on doesn't exist (either because the build never ran, or the node was deleted)

--- src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
+++ src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
@@ -36,10 +36,15 @@ public class PerforceMailResolver extends MailAddressResolver {
                 PerforceSCM pscm = (PerforceSCM) p.getScm();
                 TaskListener listener = new StreamTaskListener(System.out);
                 Node node = p.getLastBuiltOn();
+
                 // If the node is offline, skip the project.
                 // The node needs to be online for us to execute commands.
+                if (node == null) {
+                    LOGGER.finer("Build doesn't seem to have been run before. Cannot resolve email address using this project.");
+                    continue;
+                }
                 if (node.getChannel() == null) {
-                    LOGGER.finer("Node " + node.getDisplayName() + " is not up, cannot check for email using this project.");
+                    LOGGER.finer("Node " + node.getDisplayName() + " is not up, cannot resolve email address using this project.");
                     continue;
                 }
                 // TODO: replace this with p.getLastBuild().getWorkspace()
