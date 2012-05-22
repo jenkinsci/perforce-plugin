8fb04f307e9eaa5c4cdeb972a6a84f35bffb1c1d
Rob Petti <rob.petti@gmail.com>
2012-05-22 08:21:37 -0600

    fixing test failure

--- src/test/java/hudson/plugins/perforce/PerforceSCMTest.java
+++ src/test/java/hudson/plugins/perforce/PerforceSCMTest.java
@@ -284,7 +284,7 @@ public class PerforceSCMTest extends HudsonTestCase {
         String password = "pass";
         PerforceSCM scm = new PerforceSCM(
                 "user", password, "client", "port", "", "test_installation", "sysRoot",
-                "sysDrive", "label", "counter", "shared", "charset", "charset2", false, true, true, true, true, true, false,
+                "sysDrive", "label", "counter", "shared", "charset", "charset2", "user", false, true, true, true, true, true, false,
                         false, false, true, false, false, false, false, "${basename}", 0, -1, browser, "exclude_user", "exclude_file", true);
         scm.setP4Stream("stream");
         project.setScm(scm);
