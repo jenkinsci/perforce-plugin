jenkinsci/perforce-plugin

commit 6d62f20ba9cc15929d638c9e729788817c10c59c

fixing issue with relogin during command execution
If a ticket expires or authentication otherwise fails during a command, a new login attempt will be triggered. originally, this would result in a second '-P <ticket>' argument to be added to the arguments list when using tickets. this has been fixed.

src/main/java/com/tek42/perforce/parse/AbstractPerforceTemplate.java
@@ -304,14 +304,15 @@ public abstract class AbstractPerforceTemplate {
 
 		List<String> lines = null;
 		int totalLength = 0;
-
+                
+                // get entire cmd to execute
+		cmd = getExtraParams(cmd);
+                
 		do {
 			int mesgIndex = -1, count = 0;
 			Executor p4 = depot.getExecFactory().newExecutor();
 			String debugCmd = "";
-			// get entire cmd to execute
-			cmd = getExtraParams(cmd);
-
+			
 			// setup information for logging...
 			for(String cm : cmd) {
 				debugCmd += cm + " ";

commit 228084d9c717447d534466698d957500b417d950

moving getExtraParams back into main retry loop in order to fix ticket regression caused by 6d62f20ba

src/main/java/com/tek42/perforce/parse/AbstractPerforceTemplate.java
@@ -297,7 +297,7 @@ public abstract class AbstractPerforceTemplate {
             });
         }
 
-	protected StringBuilder getPerforceResponse(String cmd[], ResponseFilter filter) throws PerforceException {
+	protected StringBuilder getPerforceResponse(String origcmd[], ResponseFilter filter) throws PerforceException {
 		// TODO: Create a way to wildcard portions of the error checking.  Add method to check for these errors.
 		boolean loop = false;
 		boolean attemptLogin = true;
@@ -305,14 +305,13 @@ public abstract class AbstractPerforceTemplate {
 		List<String> lines = null;
 		int totalLength = 0;
                 
-                // get entire cmd to execute
-		cmd = getExtraParams(cmd);
-                
 		do {
 			int mesgIndex = -1, count = 0;
 			Executor p4 = depot.getExecFactory().newExecutor();
 			String debugCmd = "";
-			
+			// get entire cmd to execute
+                        String cmd[] = getExtraParams(origcmd);
+                        
 			// setup information for logging...
 			for(String cm : cmd) {
 				debugCmd += cm + " ";