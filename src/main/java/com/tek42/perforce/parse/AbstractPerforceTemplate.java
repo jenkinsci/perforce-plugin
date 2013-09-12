2178dfdc536f6d12595573a29bf3cea8df87918b
Jesse Glick <jglick@cloudbees.com>
2013-09-12 13:27:29 -0400

    [JENKINS-15904] Diagnostics for missing error messages.

--- src/main/java/com/tek42/perforce/parse/AbstractPerforceTemplate.java
+++ src/main/java/com/tek42/perforce/parse/AbstractPerforceTemplate.java
@@ -45,6 +45,7 @@ import com.tek42.perforce.PerforceException;
 import com.tek42.perforce.process.Executor;
 import hudson.plugins.perforce.PerforceSCM;
 import java.io.InputStream;
+import java.util.logging.Level;
 import org.slf4j.LoggerFactory;
 import org.apache.commons.lang.time.StopWatch;
 
@@ -131,7 +132,12 @@ public abstract class AbstractPerforceTemplate {
         public abstract static class ResponseFilter {
             public abstract boolean accept(String line);
             public boolean reject(String line){
-                return !accept(line);
+                if (accept(line)) {
+                    return false;
+                } else {
+                    java.util.logging.Logger.getLogger(AbstractPerforceTemplate.class.getName()).log(Level.FINE, "rejected output line ‘{0}’", line);
+                    return true;
+                }
             }
         }
 
