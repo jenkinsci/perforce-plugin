02350700b588acc39d774c30d43c53e7ff108d53
Oleg Nenashev <nenashev@synopsys.com>
2013-11-24 22:18:29 +0400

    [JENKINS-18583] - Refactoring of PerforceSCM to remove newInstance()
    This change removes need in newInstance() handler and also provides classes for future refactoring.
    In addition, the change adds instance IDs to config.jelly, therefore there won't be conflicts between radioButtons().
    Resolves https://issues.jenkins-ci.org/browse/JENKINS-18583
    
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

---
+++ src/main/java/hudson/plugins/perforce/config/MaskViewConfig.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 Oleg Nenashev <nenashev@synopsys.com>, Synopsys Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package hudson.plugins.perforce.config;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Defines masking options for {@link PerforceSCM}.
+ * @author Oleg Nenashev <nenashev@synopsys.com>, Synopsys Inc.
+ * @since TODO: define a version
+ */
+public class MaskViewConfig {
+    String viewMask;
+    boolean useViewMaskForPolling;
+    boolean useViewMaskForSyncing;
+
+    @DataBoundConstructor
+    public MaskViewConfig(String viewMask, boolean useViewMaskForPolling, boolean useViewMaskForSyncing) {
+        this.viewMask = viewMask;
+        this.useViewMaskForPolling = useViewMaskForPolling;
+        this.useViewMaskForSyncing = useViewMaskForSyncing;
+    }
+
+    public String getViewMask() {
+        return viewMask;
+    }
+
+    public boolean isUseViewMaskForPolling() {
+        return useViewMaskForPolling;
+    }
+
+    public boolean isUseViewMaskForSyncing() {
+        return useViewMaskForSyncing;
+    }
+}
